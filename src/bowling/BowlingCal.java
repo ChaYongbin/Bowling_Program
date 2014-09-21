c2bd1fb142ffda3e88b464395e8d5b8a088be145
chayongbin <teddy418@nhnnext.org>
2014-09-21 22:02:59 +0900

    Purpose_1_B NoneTDD

--- src/bowling/BowlingCal.java
+++ src/bowling/BowlingCal.java
@@ -2,23 +2,56 @@ package bowling;
 
 public class BowlingCal {
 	
-	int[] frmScr;
-	int[] thrBall;
-	int thrNth = 1;
+	int[] thrBall = new int [21]; // 공을 던지는 것은 한 게임당 최대 21번이다.
+	int[] frmScore = new int [10]; 
+	int count = 0;
 	
-	BowlingCal() {
-		frmScr = new int[11];
-		thrBall = new int[23];
+	public void fallPins(int pins) {
+		thrBall[count] = pins;
+		count++;
 	}
 	
-	public int run(int frm, int oneBall, int twoBall) {
-		thrBall[frm*2 - 1] = oneBall;
-		thrBall[frm*2] = twoBall;
-		frmScr[frm] = frmScr[frm - 1] + oneBall + twoBall;	
-		if (oneBall + twoBall == 10) {
-			int bonus = thrBall[frm*2+1] ;
-			return frmScr[frm] + bonus;			
+	public int frameScore(int inFrame) {
+		int frame = inFrame * 2;
+		if(inFrame - 1 == -1) {
+			if(isSpare(frame) && thrBall[frame] != 10) {
+				return thrBall[frame] + thrBall[frame + 1] + thrBall[frame + 2];
+			}
+			if(isStrike(frame) && thrBall[frame + 2] != 10) {
+				return thrBall[frame] + thrBall[frame + 2] + thrBall[frame + 3];
+			}
+			if(isNextStrike(frame)) {
+				return thrBall[frame] + thrBall[frame + 2] + thrBall[frame + 4];
+			}
+			return thrBall[frame] + thrBall[frame + 1];
 		}
-		return frmScr[frm];
+		if(isSpare(frame) && thrBall[frame] != 10) {
+			return frmScore[inFrame - 1] + thrBall[frame] + thrBall[frame + 1] + thrBall[frame + 2];
+		}
+		if(isStrike(frame) && thrBall[frame + 2] != 10) {
+			return frmScore[inFrame - 1] + thrBall[frame] + thrBall[frame + 2] + thrBall[frame + 3];
+		}
+		if(isNextStrike(frame)) {
+			return frmScore[inFrame - 1] + thrBall[frame] + thrBall[frame + 2] + thrBall[frame + 4];
+		}
+		return frmScore[inFrame - 1] + thrBall[frame] + thrBall[frame + 1];
+	}
+	
+	public int sumFrameScore() {
+		int scr = 0;
+		for(int frame = 0; frame < 10; frame++) {
+			scr += frmScore[frame];
+		}
+		return scr;
+	}
+	
+	public Boolean isSpare(int frame) {
+		return thrBall[frame] + thrBall[frame + 1] == 10;
+	}
+	public Boolean isStrike(int frame) {
+		return thrBall[frame] == 10;
+	}
+	public Boolean isNextStrike(int frame) {
+		return thrBall[frame + 2] == 10;
 	}
 }
--- test/bowling/BowlingCalTest.java
+++ test/bowling/BowlingCalTest.java
@@ -3,21 +3,62 @@ package bowling;
 import junit.framework.TestCase;
 
 public class BowlingCalTest extends TestCase {
-	
-	public void testCalculatorOneFrame() throws Exception {
+	public void testSpare() throws Exception {
 		BowlingCal cal = new BowlingCal();
+		cal.fallPins(9);
+		cal.fallPins(1);
+		cal.fallPins(8);
+		cal.fallPins(1);
+		cal.fallPins(0);
+		cal.fallPins(5);
+		cal.fallPins(0);
+		cal.fallPins(10);
+		cal.fallPins(1);
+		cal.fallPins(4);
+		cal.fallPins(5);
+		cal.fallPins(2);
+		cal.fallPins(9);
+		cal.fallPins(1);
+		cal.fallPins(3);
+		cal.fallPins(4);
+		cal.fallPins(0);
+		cal.fallPins(10);
+		cal.fallPins(1);
+		cal.fallPins(8);
 		
-		int target = cal.run(1, 3, 4);
-		assertEquals(7, target);
-	}
-	public void testCalculatorTwoFrameSpare() throws Exception {
-		BowlingCal cal = new BowlingCal();
+		int goal = cal.frameScore(0);
+		assertEquals(18, goal);
 		
-		cal.run(1, 9, 1);
-		assertEquals(10, cal.frmScr[1]);
-		cal.run(2, 8, 1);
-		assertEquals(18,cal.frmScr[1]);
-		assertEquals(27, cal.frmScr[2]);
+		goal = cal.frameScore(1);
+		assertEquals(27, goal);
+		
+		goal = cal.frameScore(2);
+		assertEquals(32, goal);
+		
+		goal = cal.frameScore(3);
+		assertEquals(43, goal);
+		
+		goal = cal.frameScore(4);
+		assertEquals(48, goal);
+		
+		goal = cal.frameScore(5);
+		assertEquals(55, goal);
+		
+		goal = cal.frameScore(6);
+		assertEquals(68, goal);
+		
+		goal = cal.frameScore(7);
+		assertEquals(75, goal);
+		
+		goal = cal.frameScore(8);
+		assertEquals(86, goal);
+		
+		goal = cal.frameScore(9);
+		assertEquals(95, goal);
+		
+		goal = cal.sumFrameScore();
+		assertEquals(95, goal);
+		
 	}
 }
 
